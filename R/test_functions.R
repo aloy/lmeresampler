6410481375285e1ab8164239c70307812449802c
Spenser Steele <spenser.steele@gmail.com>
2014-06-27 16:23:43 -0500

    Added function shells

--- R/test_functions.R
+++ R/test_functions.R
@@ -11,3 +11,37 @@ boot <- function (model, fn, type){
          reb1 = reb(model, fn, reb_type = 1),
          reb2 = reb(model, fn, reb_type = 2))
 }
+
+parametric <- function (model, fn){
+  
+}
+
+residual <- function (model, fn){
+  
+}
+
+case <- function (model, fn){
+  
+}
+
+cgr <- function (model, fn){
+  
+}
+
+reb <- function (model, fn, reb_type){
+  if(reb_type = 1){
+    # Call reb1 here
+  }
+  # reb code here
+  if(reb_type = 2){
+    # Call reb2 here
+  }
+}
+
+reb1 <- function (model, fn){
+  
+}
+
+reb2 <- function (model, fn){
+  
+}
