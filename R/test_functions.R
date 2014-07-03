b5d03d088bd15efd5ff8328d2cd32291b06d3e4d
Spenser Steele <spenser.steele@gmail.com>
2014-07-03 09:42:59 -0500

    Removed unnecessary comment

--- R/test_functions.R
+++ R/test_functions.R
@@ -73,8 +73,6 @@ parametric.lmerMod <- function(model, fn, B){
   model.fixef <- fixef(model) # Extract fixed effects
   # fn.star <- rep(0, B)
   # model.star <- c(1:B) # we don't need to initialize this is we use apply statements
-  # Can I just simulate multiple times and run refit once on the entire array
-  # to just get a returned array
   y.star <- simulate(model, nsim = B)
   # Below is one idea that will be compatible with the boot package (for CIs)
   t0 <- fn(model)
