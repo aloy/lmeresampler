902848dad497a4fb30d84fc5662b2d9d5851faa1
Spenser Steele <spenser.steele@gmail.com>
2014-07-08 10:48:14 -0500

    updated test

--- R/for_testing_usage.R
+++ R/for_testing_usage.R
@@ -2,5 +2,8 @@
 
 # Data set
 data(sleepstudy)
+# fm1 model
 (fm1 <- lmer(Reaction ~ Days + (Days | Subject), data = sleepstudy))
+
+# Parametric test
 parametric.lmerMod(model = fm1, fn = fixef, B = 1000)
