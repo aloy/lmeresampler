b37cbc494197e8b3205dd15e68fc4f56356a5ef1
Spenser Steele <spenser.steele@gmail.com>
2014-07-10 15:43:38 -0500

    tested bootstrap function

--- R/for_testing_usage.R
+++ R/for_testing_usage.R
@@ -5,5 +5,9 @@ data(sleepstudy)
 # fm1 model
 (fm1 <- lmer(Reaction ~ Days + (Days | Subject), data = sleepstudy))
 
+
+# Bootstrap function test
+fm1.res <- bootstrap(model = fm1, fn = fixef, type = "par", B = 100)
+
 # Parametric test
-parametric.lmerMod(model = fm1, fn = fixef, B = 100)
+fm1.par.res <- parametric.lmerMod(model = fm1, fn = fixef, B = 100)
