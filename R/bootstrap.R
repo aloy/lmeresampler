d04ae6af027c4db390c78eed4262c040bf297fb0
Spenser Steele <spenser.steele@gmail.com>
2014-07-29 13:15:46 -0500

    added oop

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -210,7 +210,7 @@ cgr.lmerMod <- function (model, fn, B){
   Ls <- chol(S, pivot = TRUE)
   # ISS: R needs to be a matrix for this to work
   Lr <- chol(R, pivot = TRUE)
-  A <- t(Lr%*%Ls^(-1))
+  A <- t(Lr%*%(Ls^(-1)))
   
   # center the scaled residuals at zero
   # also use an lapply here to do this, unless there is a faster way
