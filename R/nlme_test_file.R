a7e3ed6e114f8d68d420e6dec277df4151972f71
Spenser Steele <spenser.steele@gmail.com>
2014-08-19 10:26:22 -0500

    tested single case, worked

--- R/nlme_test_file.R
+++ R/nlme_test_file.R
@@ -24,10 +24,40 @@ t0 <- fn(model)
 # Error in eval(expr, envir, enclos) : object 'x' not found
 # And when I change it to Reaction it runs but I do not think it works
 # Refit the model and apply 'fn' to it using lapply
-tstar <- lapply(ystar, function(Reaction) {
-  update(object = model, Reaction ~ .)
-  fn(model)
+tstar <- lapply(ystar, function(x) {
+  model.update <- update(object = model, x ~ .)
+  t.res <- fn(model.update)
+  return(t.res)
 })
+
+tstar <- do.call("cbind", tstar) # Can these be nested?
+rownames(tstar) <- names(t0)
+
+RES <- structure(list(t0 = t0, t = t(tstar), R = B, data = model@frame,
+                      seed = .Random.seed, statistic = fn,
+                      sim = "parametric", call = match.call()),
+                 class = "boot")
+
+
+
+
+####### Singular test ######
+model <- lme(Reaction ~ Days, data = sleepstudy, random = ~Days|Subject)
+fn <- fixed.effects
+B <- 1
+
+### BEGIN PAR CODE ###
+fn <- match.fun(fn)
+
+model.fixef <- fixed.effects(model) # Extract fixed effects
+ystar <- simulate.lme.data(model, nsim = B, na.action = na.exclude)
+
+t0 <- fn(model)
+
+# Refit the model and apply 'fn' to it using lapply
+model.update <- update(object = model, ystar ~ .)
+anova(model.update, model) # This should not run if the update worked
+tstar <- fn(model.update)
 
 tstar <- do.call("cbind", tstar) # Can these be nested?
 rownames(tstar) <- names(t0)
