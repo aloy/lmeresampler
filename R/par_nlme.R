7ae5bf21725fea20a44a7085e3d08d0c391950d1
aloy <loyad01@gmail.com>
2014-08-25 09:39:05 -0500

    functionalized parametric.lme

--- R/par_nlme.R
+++ R/par_nlme.R
@@ -1,27 +1,30 @@
-# Match function
-fn <- match.fun(fn)
-# Extract fixed effects
-model.fixef <- fixed.effects(model)
-
-ystar <- simulateY(model, nsim = B)
-row.names(ystar) <- 1:model$dims$N
-
-t0 <- fn(model)
+parametric.lme <- function(model, fn, B){
+  # Match function
+  fn <- match.fun(fn)
+  # Extract fixed effects
+  model.fixef <- fixed.effects(model)
   
-t.res <- matrix(0, ncol = 2, nrow = B)
-for(i in 1:B){
-  myin <- ystar[,i]
-  model.update <- update(object = model, fixed = myin ~ .)
-  t.res[i,] <- fn(model.update)
+  ystar <- simulateY(model, nsim = B)
+  row.names(ystar) <- 1:model$dims$N
+  
+  t0 <- fn(model)
+  
+  t.res <- matrix(0, ncol = 2, nrow = B)
+  for(i in 1:B){
+    myin <- ystar[,i]
+    model.update <- update(object = model, fixed = myin ~ .)
+    t.res[i,] <- fn(model.update)
+  }
+  tstar <- data.frame(t(t.res))
+
+#   tstar <- split(t.res, rep(1:ncol(t.res), each = nrow(t.res)))
+#   
+#   tstar <- do.call("cbind", tstar) # Can these be nested?
+  rownames(tstar) <- names(t0)
+  colnames(tstar) <- 1:ncol(tstar)
+  
+  RES <- structure(list(t0 = t0, t = t(tstar), R = B, data = model$data,
+                        seed = .Random.seed, statistic = fn,
+                        sim = "parametric", call = match.call()),
+                   class = "boot")
 }
-t.res <- t(t.res)
-tstar <- split(t.res, rep(1:ncol(t.res), each = nrow(t.res)))
-  
-  
-tstar <- do.call("cbind", tstar) # Can these be nested?
-rownames(tstar) <- names(t0)
-  
-RES <- structure(list(t0 = t0, t = t(tstar), R = B, data = model$data,
-                      seed = .Random.seed, statistic = fn,
-                      sim = "parametric", call = match.call()),
-                 class = "boot")
