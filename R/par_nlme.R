8e77b611b00f1ec3fba816dfd17419eef6a62352
Spenser Steele <spenser.steele@gmail.com>
2014-08-26 09:35:12 -0700

    Added updated.model fn
    This function should update the model

--- R/par_nlme.R
+++ R/par_nlme.R
@@ -9,11 +9,15 @@ parametric.lme <- function(model, fn, B){
   
   t0 <- fn(model)
   
-  t.res <- matrix(0, ncol = 2, nrow = B)
+   t.res <- matrix(0, ncol = 2, nrow = B)
+#   for(i in 1:B){
+#     myin <- ystar[,i]
+#     model.update <- update(object = model, fixed = myin ~ .)
+#     t.res[i,] <- fn(model.update)
+#   }
+
   for(i in 1:B){
-    myin <- ystar[,i]
-    model.update <- update(object = model, fixed = myin ~ .)
-    t.res[i,] <- fn(model.update)
+    t.res[i,] <- updated.model(model = model, up.reaction = ystar[,i])
   }
   tstar <- data.frame(t(t.res))
 
@@ -27,4 +31,16 @@ parametric.lme <- function(model, fn, B){
                         seed = .Random.seed, statistic = fn,
                         sim = "parametric", call = match.call()),
                    class = "boot")
+}
+
+updated.model<- function(model, up.reaction){
+  # Extract formulas and data
+  mod.fixd <- as.formula(model$call$fixed)
+  mod.rand <- as.formula(model$call$random)
+  mod.data <- model$data
+  # Place ystars in data
+  mod.data$Reaction <- up.reaction
+  # create new lme
+  out.lme <- lme(fixed = mod.fixd, data = mod.data, random = mod.rand)
+  return(out.lme)
 }
