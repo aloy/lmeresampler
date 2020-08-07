29474800552cfc5751b23c212917bff91f79ec5a
Jenna Korobova <korobovaj@carleton.edu>
2020-08-07 16:56:32 -0500

    general edits, combine working

--- R/combine_lmersamp.R
+++ R/combine_lmersamp.R
@@ -13,9 +13,13 @@
 combine_lmeresamp <- function(...) {
   boot_list <- list(...)
   combo_replicates <- purrr::map_dfr(boot_list, ~as.data.frame(.x$replicates))
-  combo_stats <- sum(map_dbl(boot_list, ~.x$stats)) #change stats
+  combo_r <- sum(map_dbl(boot_list, ~.x$R))
   RES <- boot_list[[1]]
+  RES$R <- combo_r
   RES$replicates <- combo_replicates
-  RES$stats <- combo_stats
+  RES$stats$rep.mean <- colMeans(RES$replicates) # recalculated mean
+  RES$stats$se <- unlist(purrr::map(RES$replicates, sd)) # recalculated se
+  RES$stats$bias <- RES$stats$rep.mean - RES$stats$observed # recalculated bias
   return(RES)
 }
+
