c51a4317ea08da51458d302eba163c99e638653a
adam <loyad01@gmail.com>
2021-03-24 08:46:29 -0500

    hacnged x to object for method consistency

--- R/summary.R
+++ R/summary.R
@@ -4,25 +4,24 @@
 #' Summarizes the output of the bootstrap call.
 #'
 #' @details
-#' This function is given \code{x, ci} and uses them to print the bootstrap result
+#' This function is given \code{object, ci} and uses them to print the bootstrap result
 #' and confidence intervals if the user wishes to have them.
 #'
-#' @param x The lmeresamp object to print.
-#' @param ci A logical value specifying whether confidence intervals should be printed.
+#' @param object ci A logical value specifying whether confidence intervals should be printed.
 #' @param ... not used
 #'
 #' @rdname summary
 #' @export 
 #' @method summary lmeresamp
-summary.lmeresamp <- function(x, ci = FALSE, ...){
+summary.lmeresamp <- function(object, ci = FALSE, ...){
   
-  cat(paste("Bootstrap type:", x$type, "\n"))
+  cat(paste("Bootstrap type:", object$type, "\n"))
   cat(paste("\n"))
-  cat(paste("Number of resamples:", x$R, "\n"))
+  cat(paste("Number of resamples:", object$R, "\n"))
   cat(paste("\n"))
-  print(x$stats)
+  print(object$stats)
   
   if(ci == TRUE){
-    confint(x)
+    confint(object)
   }
 }
