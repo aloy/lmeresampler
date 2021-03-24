91c83561e957e0c80d4bae1a2072e6f09d286222
Jenna Korobova <korobovaj@carleton.edu>
2021-03-24 12:19:52 -0500

    summary method

--- R/summary.R
+++ R/summary.R
@@ -7,21 +7,45 @@
 #' This function is given \code{object, ci} and uses them to print the bootstrap result
 #' and confidence intervals if the user wishes to have them.
 #'
-#' @param object ci A logical value specifying whether confidence intervals should be printed.
+#' @param object The lmeresamp object to be summarized.
 #' @param ... not used
 #'
 #' @rdname summary
 #' @export 
 #' @method summary lmeresamp
-summary.lmeresamp <- function(object, ci = FALSE, ...){
+summary.lmeresamp <- function(object, ...){
+  
+  # use this to test warnings
+  # lme_res_boot <- bootstrap(vcmodB, .f = fixef, type = "residual", B = 100)
   
   cat(paste("Bootstrap type:", object$type, "\n"))
   cat(paste("\n"))
   cat(paste("Number of resamples:", object$R, "\n"))
   cat(paste("\n"))
   print(object$stats)
+  cat(paste("\n"))
   
-  if(ci == TRUE){
-    confint(object)
+  message_count <- 0
+  for(i in length(object$message)){
+    if(!is.null(object$message[i])){
+      message_count <- message_count + 1
+    }
   }
+  
+  warning_count <- 0
+  for(j in length(object$warning)){
+    if(!is.null(object$warning[j])){
+      warning_count <- warning_count + 1
+    }
+  }
+  
+  error_count <- 0
+  for(k in length(object$error)){
+    if(!is.null(object$error[k])){
+      error_count <- error_count + 1
+    }
+  }
+  
+  cat(paste("There were", message_count, "messages,", warning_count, "warnings, and", error_count, "errors."))
+  
 }
