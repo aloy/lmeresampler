9c21057b11229f2149b9e02359f794430e4d1019
Jenna Korobova <korobovaj@carleton.edu>
2021-03-29 12:36:22 -0500

    summary done!

--- R/summary.R
+++ R/summary.R
@@ -18,34 +18,53 @@ summary.lmeresamp <- function(object, ...){
   # use this to test warnings
   # lme_res_boot <- bootstrap(vcmodB, .f = fixef, type = "residual", B = 100)
   
-  cat(paste("Bootstrap type:", object$type, "\n"))
-  cat(paste("\n"))
-  cat(paste("Number of resamples:", object$R, "\n"))
-  cat(paste("\n"))
-  print(object$stats)
-  cat(paste("\n"))
+  print.lmeresamp(object)
   
   message_count <- 0
   for(i in length(object$message)){
-    if(!is.null(object$message[i])){
+    if(!is.null(object$message[[i]])){
       message_count <- message_count + 1
     }
   }
   
   warning_count <- 0
   for(j in length(object$warning)){
-    if(!is.null(object$warning[j])){
+    if(!is.null(object$warning[[j]])){
       warning_count <- warning_count + 1
     }
   }
   
   error_count <- 0
   for(k in length(object$error)){
-    if(!is.null(object$error[k])){
+    if(!is.null(object$error[[k]])){
       error_count <- error_count + 1
     }
   }
   
+  cat(paste("\n"))
   cat(paste("There were", message_count, "messages,", warning_count, "warnings, and", error_count, "errors."))
+  cat(paste("\n"))
+  
+  # finding most commonly occuring message/warning/error
+  object$message <- as.factor(object$message)
+  object$warning <- as.factor(object$warning)
+  object$error <- as.factor(object$error)
+  
+  top_message <- names(sort(summary(object$message), decreasing=T)[[1]])
+  if(!is.null(top_message)){
+    cat(paste("The most commonly occuring message was:"))
+    cat(paste("\n"))
+  }
+  
+  top_warning <- names(sort(summary(object$warning), decreasing=T)[[1]])
+  if(!is.null(top_warning)){
+    cat(paste("The most commonly occuring warning was:"))
+    cat(paste("\n"))
+  }
+  
+  top_error <- names(sort(summary(object$error), decreasing=T)[[1]])
+  if(!is.null(top_error)){
+    cat(paste("The most commonly occuring error was:"))
+  }
   
 }
