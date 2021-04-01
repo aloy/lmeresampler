8180bf9bfeb95b16c8335589d4231e6b6541ae88
Jenna Korobova <korobovaj@carleton.edu>
2021-03-31 22:48:59 -0500

    updated wild bootstrap tests

--- tests/testthat/test-wild-nlme.R
+++ tests/testthat/test-wild-nlme.R
@@ -8,7 +8,7 @@ Socatt$rv <- scale(Socatt$rv) # a plot shows this is clearly non-normal
 
 
 # ==============================================================================
-context("wild bootstrap (lme)")
+context("wild bootstrap hccme = hc2 aux.dist = f1 (lme)")
 # ==============================================================================
 
 mySumm <- function(.) { 
@@ -25,7 +25,7 @@ test_that("two-level additive random intercept model",{
   
   orig.stats <- mySumm(vcmodA)
   
-  boo <- wild_bootstrap(model = vcmodA, .f = mySumm, B = nsim)
+  boo <- wild_bootstrap(model = vcmodA, .f = mySumm, B = nsim, hccme = "hc2", aux.dist = "f1")
   
   expect_equal(class(boo), "lmeresamp")
   expect_equal(boo$observed, orig.stats)   
@@ -46,7 +46,7 @@ test_that("two-level random intercept model without interaction",{
                random = ~ 1 | school, data = jsp728)
   
   orig.stats <- mySumm(rimod)
-  boo <- wild_bootstrap(model = rimod, .f = mySumm, B = nsim)
+  boo <- wild_bootstrap(model = rimod, .f = mySumm, B = nsim, hccme = "hc2", aux.dist = "f1")
   
   expect_equal(class(boo), "lmeresamp")
   expect_equal(boo$observed, orig.stats)   
@@ -66,7 +66,7 @@ test_that("two-level random intercept model with interaction",{
                 random = ~ 1 | school, data = jsp728)
   
   orig.stats <- mySumm(vcmodC)
-  boo <- wild_bootstrap(model = vcmodC, .f = mySumm, B = nsim)
+  boo <- wild_bootstrap(model = vcmodC, .f = mySumm, B = nsim, hccme = "hc2", aux.dist = "f1")
   
   expect_equal(class(boo), "lmeresamp")
   expect_equal(boo$observed, orig.stats)   
@@ -87,7 +87,283 @@ test_that("two-level random coefficient model with interaction",{
                random = ~ mathAge8c | school, data = jsp728)
   
   orig.stats <- mySumm(rcmod)
-  boo <- wild_bootstrap(model = rcmod, .f = mySumm, B = nsim)
+  boo <- wild_bootstrap(model = rcmod, .f = mySumm, B = nsim, hccme = "hc2", aux.dist = "f1")
+  
+  expect_equal(class(boo), "lmeresamp")
+  expect_equal(boo$observed, orig.stats)   
+  expect_equal(unname(boo$stats$observed), unname(orig.stats))
+  expect_equal(nrow(boo$replicates), nsim)
+  expect_equal(ncol(boo$replicates), length(orig.stats))
+  expect_equal(boo$B, nsim)
+  expect_equal(boo$type, "wild")
+  expect_equal(boo$.f, mySumm)
+})
+
+# ==============================================================================
+context("wild bootstrap hccme = hc2 aux.dist = f2 (lme)")
+# ==============================================================================
+
+mySumm <- function(.) { 
+  c(beta = fixef(.), sigma = as.numeric(.$sigma), sig01 = as.numeric(VarCorr(.)[1,2]))
+}
+
+nsim <- 10
+
+test_that("two-level additive random intercept model",{
+  skip_on_cran()
+  ## See p. 31 of Goldstein's book
+  vcmodA <- lme(mathAge11 ~ mathAge8 + gender + class,
+                random = ~ 1 | school, data = jsp728)
+  
+  orig.stats <- mySumm(vcmodA)
+  
+  boo <- wild_bootstrap(model = vcmodA, .f = mySumm, B = nsim, hccme = "hc2", aux.dist = "f2")
+  
+  expect_equal(class(boo), "lmeresamp")
+  expect_equal(boo$observed, orig.stats)   
+  expect_equal(unname(boo$stats$observed), unname(orig.stats))
+  expect_equal(nrow(boo$replicates), nsim)
+  expect_equal(ncol(boo$replicates), length(orig.stats))
+  expect_equal(boo$B, nsim)
+  expect_equal(boo$type, "wild")
+  expect_equal(boo$.f, mySumm)
+})
+
+# ------------------------------------------------------------------------------
+
+test_that("two-level random intercept model without interaction",{
+  skip_on_cran()
+  ## See p. 97 of Goldstein's book
+  rimod <- lme(mathAge11 ~ mathAge8c + gender + class,
+               random = ~ 1 | school, data = jsp728)
+  
+  orig.stats <- mySumm(rimod)
+  boo <- wild_bootstrap(model = rimod, .f = mySumm, B = nsim, hccme = "hc2", aux.dist = "f2")
+  
+  expect_equal(class(boo), "lmeresamp")
+  expect_equal(boo$observed, orig.stats)   
+  expect_equal(unname(boo$stats$observed), unname(orig.stats))
+  expect_equal(nrow(boo$replicates), nsim)
+  expect_equal(ncol(boo$replicates), length(orig.stats))
+  expect_equal(boo$B, nsim)
+  expect_equal(boo$type, "wild")
+  expect_equal(boo$.f, mySumm)
+})
+
+
+test_that("two-level random intercept model with interaction",{
+  skip_on_cran()
+  ## See p. 34 of Goldstein's book
+  vcmodC <- lme(mathAge11 ~ mathAge8 * schoolMathAge8 + gender + class, 
+                random = ~ 1 | school, data = jsp728)
+  
+  orig.stats <- mySumm(vcmodC)
+  boo <- wild_bootstrap(model = vcmodC, .f = mySumm, B = nsim, hccme = "hc2", aux.dist = "f2")
+  
+  expect_equal(class(boo), "lmeresamp")
+  expect_equal(boo$observed, orig.stats)   
+  expect_equal(unname(boo$stats$observed), unname(orig.stats))
+  expect_equal(nrow(boo$replicates), nsim)
+  expect_equal(ncol(boo$replicates), length(orig.stats))
+  expect_equal(boo$B, nsim)
+  expect_equal(boo$type, "wild")
+  expect_equal(boo$.f, mySumm)
+})
+
+# ------------------------------------------------------------------------------
+
+test_that("two-level random coefficient model with interaction",{
+  skip_on_cran()
+  ## See p. 35 of Goldstein's book
+  rcmod <- lme(mathAge11 ~ mathAge8c * schoolMathAge8 + gender + class,
+               random = ~ mathAge8c | school, data = jsp728)
+  
+  orig.stats <- mySumm(rcmod)
+  boo <- wild_bootstrap(model = rcmod, .f = mySumm, B = nsim, hccme = "hc2", aux.dist = "f2")
+  
+  expect_equal(class(boo), "lmeresamp")
+  expect_equal(boo$observed, orig.stats)   
+  expect_equal(unname(boo$stats$observed), unname(orig.stats))
+  expect_equal(nrow(boo$replicates), nsim)
+  expect_equal(ncol(boo$replicates), length(orig.stats))
+  expect_equal(boo$B, nsim)
+  expect_equal(boo$type, "wild")
+  expect_equal(boo$.f, mySumm)
+})
+
+# ==============================================================================
+context("wild bootstrap hccme = hc3 aux.dist = f1 (lme)")
+# ==============================================================================
+
+mySumm <- function(.) { 
+  c(beta = fixef(.), sigma = as.numeric(.$sigma), sig01 = as.numeric(VarCorr(.)[1,2]))
+}
+
+nsim <- 10
+
+test_that("two-level additive random intercept model",{
+  skip_on_cran()
+  ## See p. 31 of Goldstein's book
+  vcmodA <- lme(mathAge11 ~ mathAge8 + gender + class,
+                random = ~ 1 | school, data = jsp728)
+  
+  orig.stats <- mySumm(vcmodA)
+  
+  boo <- wild_bootstrap(model = vcmodA, .f = mySumm, B = nsim, hccme = "hc3", aux.dist = "f1")
+  
+  expect_equal(class(boo), "lmeresamp")
+  expect_equal(boo$observed, orig.stats)   
+  expect_equal(unname(boo$stats$observed), unname(orig.stats))
+  expect_equal(nrow(boo$replicates), nsim)
+  expect_equal(ncol(boo$replicates), length(orig.stats))
+  expect_equal(boo$B, nsim)
+  expect_equal(boo$type, "wild")
+  expect_equal(boo$.f, mySumm)
+})
+
+# ------------------------------------------------------------------------------
+
+test_that("two-level random intercept model without interaction",{
+  skip_on_cran()
+  ## See p. 97 of Goldstein's book
+  rimod <- lme(mathAge11 ~ mathAge8c + gender + class,
+               random = ~ 1 | school, data = jsp728)
+  
+  orig.stats <- mySumm(rimod)
+  boo <- wild_bootstrap(model = rimod, .f = mySumm, B = nsim, hccme = "hc3", aux.dist = "f1")
+  
+  expect_equal(class(boo), "lmeresamp")
+  expect_equal(boo$observed, orig.stats)   
+  expect_equal(unname(boo$stats$observed), unname(orig.stats))
+  expect_equal(nrow(boo$replicates), nsim)
+  expect_equal(ncol(boo$replicates), length(orig.stats))
+  expect_equal(boo$B, nsim)
+  expect_equal(boo$type, "wild")
+  expect_equal(boo$.f, mySumm)
+})
+
+
+test_that("two-level random intercept model with interaction",{
+  skip_on_cran()
+  ## See p. 34 of Goldstein's book
+  vcmodC <- lme(mathAge11 ~ mathAge8 * schoolMathAge8 + gender + class, 
+                random = ~ 1 | school, data = jsp728)
+  
+  orig.stats <- mySumm(vcmodC)
+  boo <- wild_bootstrap(model = vcmodC, .f = mySumm, B = nsim, hccme = "hc3", aux.dist = "f1")
+  
+  expect_equal(class(boo), "lmeresamp")
+  expect_equal(boo$observed, orig.stats)   
+  expect_equal(unname(boo$stats$observed), unname(orig.stats))
+  expect_equal(nrow(boo$replicates), nsim)
+  expect_equal(ncol(boo$replicates), length(orig.stats))
+  expect_equal(boo$B, nsim)
+  expect_equal(boo$type, "wild")
+  expect_equal(boo$.f, mySumm)
+})
+
+# ------------------------------------------------------------------------------
+
+test_that("two-level random coefficient model with interaction",{
+  skip_on_cran()
+  ## See p. 35 of Goldstein's book
+  rcmod <- lme(mathAge11 ~ mathAge8c * schoolMathAge8 + gender + class,
+               random = ~ mathAge8c | school, data = jsp728)
+  
+  orig.stats <- mySumm(rcmod)
+  boo <- wild_bootstrap(model = rcmod, .f = mySumm, B = nsim, hccme = "hc3", aux.dist = "f1")
+  
+  expect_equal(class(boo), "lmeresamp")
+  expect_equal(boo$observed, orig.stats)   
+  expect_equal(unname(boo$stats$observed), unname(orig.stats))
+  expect_equal(nrow(boo$replicates), nsim)
+  expect_equal(ncol(boo$replicates), length(orig.stats))
+  expect_equal(boo$B, nsim)
+  expect_equal(boo$type, "wild")
+  expect_equal(boo$.f, mySumm)
+})
+
+# ==============================================================================
+context("wild bootstrap hccme = hc3 aux.dist = f2 (lme)")
+# ==============================================================================
+
+mySumm <- function(.) { 
+  c(beta = fixef(.), sigma = as.numeric(.$sigma), sig01 = as.numeric(VarCorr(.)[1,2]))
+}
+
+nsim <- 10
+
+test_that("two-level additive random intercept model",{
+  skip_on_cran()
+  ## See p. 31 of Goldstein's book
+  vcmodA <- lme(mathAge11 ~ mathAge8 + gender + class,
+                random = ~ 1 | school, data = jsp728)
+  
+  orig.stats <- mySumm(vcmodA)
+  
+  boo <- wild_bootstrap(model = vcmodA, .f = mySumm, B = nsim, hccme = "hc3", aux.dist = "f2")
+  
+  expect_equal(class(boo), "lmeresamp")
+  expect_equal(boo$observed, orig.stats)   
+  expect_equal(unname(boo$stats$observed), unname(orig.stats))
+  expect_equal(nrow(boo$replicates), nsim)
+  expect_equal(ncol(boo$replicates), length(orig.stats))
+  expect_equal(boo$B, nsim)
+  expect_equal(boo$type, "wild")
+  expect_equal(boo$.f, mySumm)
+})
+
+# ------------------------------------------------------------------------------
+
+test_that("two-level random intercept model without interaction",{
+  skip_on_cran()
+  ## See p. 97 of Goldstein's book
+  rimod <- lme(mathAge11 ~ mathAge8c + gender + class,
+               random = ~ 1 | school, data = jsp728)
+  
+  orig.stats <- mySumm(rimod)
+  boo <- wild_bootstrap(model = rimod, .f = mySumm, B = nsim, hccme = "hc3", aux.dist = "f2")
+  
+  expect_equal(class(boo), "lmeresamp")
+  expect_equal(boo$observed, orig.stats)   
+  expect_equal(unname(boo$stats$observed), unname(orig.stats))
+  expect_equal(nrow(boo$replicates), nsim)
+  expect_equal(ncol(boo$replicates), length(orig.stats))
+  expect_equal(boo$B, nsim)
+  expect_equal(boo$type, "wild")
+  expect_equal(boo$.f, mySumm)
+})
+
+
+test_that("two-level random intercept model with interaction",{
+  skip_on_cran()
+  ## See p. 34 of Goldstein's book
+  vcmodC <- lme(mathAge11 ~ mathAge8 * schoolMathAge8 + gender + class, 
+                random = ~ 1 | school, data = jsp728)
+  
+  orig.stats <- mySumm(vcmodC)
+  boo <- wild_bootstrap(model = vcmodC, .f = mySumm, B = nsim, hccme = "hc3", aux.dist = "f2")
+  
+  expect_equal(class(boo), "lmeresamp")
+  expect_equal(boo$observed, orig.stats)   
+  expect_equal(unname(boo$stats$observed), unname(orig.stats))
+  expect_equal(nrow(boo$replicates), nsim)
+  expect_equal(ncol(boo$replicates), length(orig.stats))
+  expect_equal(boo$B, nsim)
+  expect_equal(boo$type, "wild")
+  expect_equal(boo$.f, mySumm)
+})
+
+# ------------------------------------------------------------------------------
+
+test_that("two-level random coefficient model with interaction",{
+  skip_on_cran()
+  ## See p. 35 of Goldstein's book
+  rcmod <- lme(mathAge11 ~ mathAge8c * schoolMathAge8 + gender + class,
+               random = ~ mathAge8c | school, data = jsp728)
+  
+  orig.stats <- mySumm(rcmod)
+  boo <- wild_bootstrap(model = rcmod, .f = mySumm, B = nsim, hccme = "hc3", aux.dist = "f2")
   
   expect_equal(class(boo), "lmeresamp")
   expect_equal(boo$observed, orig.stats)   
