6801ebc7dcc2f211d1e8a15df787ea7ec80af5b9
adam <loyad01@gmail.com>
2021-07-27 12:28:05 -0500

    fixing error/message/warning bug in summary()

--- R/summary.R
+++ R/summary.R
@@ -28,35 +28,20 @@ summary.lmeresamp <- function(object, ...){
   cat(paste("\n"))
   print(as.data.frame(object$stats))
   
-  message_count <- 0
-  for(i in length(object$message)){
-    if(!is.null(object$message[[i]])){
-      message_count <- message_count + 1
-    }
-  }
-  
-  warning_count <- 0
-  for(j in length(object$warning)){
-    if(!is.null(object$warning[[j]])){
-      warning_count <- warning_count + 1
-    }
-  }
-  
-  error_count <- 0
-  for(k in length(object$error)){
-    if(!is.null(object$error[[k]])){
-      error_count <- error_count + 1
-    }
-  }
+  messages <- unlist(object$message)
+  warnings <- unlist(object$warnings)
+  errors   <- unlist(object$error)
   
   cat(paste("\n"))
-  cat(paste("There were", message_count, "messages,", warning_count, "warnings, and", error_count, "errors."))
+  cat(paste("There were", length(messages), "messages,", 
+            length(warnings), "warnings, and", 
+            length(errors), "errors."))
   cat(paste("\n"))
   
   # finding most commonly occuring message/warning/error
-  object$message <- as.factor(object$message)
-  object$warning <- as.factor(object$warning)
-  object$error <- as.factor(object$error)
+  object$message <- as.factor(messages)
+  object$warning <- as.factor(warnings)
+  object$error <- as.factor(errors)
   
   top_message <- names(sort(summary(object$message), decreasing=T)[1])
   if(!is.null(top_message)){
